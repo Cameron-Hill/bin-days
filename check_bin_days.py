1bb288fe2d0e5a50e7ed255b5b8be57bc1f98187
Cameron-Hill <cameronhill347@gmail.com>
2022-12-10 21:01:50 +0000

    fetch url page

--- check_bin_days.py
+++ check_bin_days.py
@@ -6,12 +6,15 @@ import pickle
 
 BIN_DAYS_URL = 'https://www.south-ayrshire.gov.uk/bin-days'
 BIN_DAYS_POSTCODE_VAR = 'BINDAYS_PAGE1_POSTCODE'
+BIN_DAYS_FORM_ID = 'BINDAYS_FORM'
 session = HTMLSession()
 
 
 def fetch_postcode_form():
-    BIN_DAYS_FORM_ID = 'BINDAYS_FORM'
+    print("Fetching Postcode form...")
     # response = session.get(BIN_DAYS_URL)
+    # with open('./fetch_postcode_form_response.pk', 'wb') as f:
+        # pickle.dump(response, f)
     with open('./fetch_postcode_form_response.pk', 'rb') as f:
         response = pickle.load(f)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -19,19 +22,57 @@ def fetch_postcode_form():
     return form
 
 
-def get_postcode_form_input_details(postcode_form, postcode):
-    inputs = []
-    for input in postcode_form.find_all("input"):
-        d = {}
-        for key, value in input.attrs.items():
-            d[key] = value
-            if key in insert:
-                d[key] = insert[key]
-        inputs.append(d)
+def get_payload_from_form(form):
+    payload = {}
+    search_term = ["input", "button"]
+    for term in search_term:
+        for input in form.find_all(term):
+            if input.get("value"):
+                payload[input["name"]] = input["value"]
+    return payload
 
 
 def fetch_address_form(postcode_form, postcode):
-    input_details = get_postcode_form_input_details(postcode_form, postcode)
+    print(f"Fetching address form from postcode form : {postcode}")
+    payload = get_payload_from_form(postcode_form)
+    payload[BIN_DAYS_POSTCODE_VAR] = postcode
+    url = postcode_form.get("action")
+    # response = session.post(url, data=payload, allow_redirects=True)
+    # with open("./fetch_address_form_response.pk", 'wb') as f:
+    #     pickle.dump(response, f)
+    with open("./fetch_address_form_response.pk", 'rb') as f:
+        response = pickle.load(f)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    form = soup.find("form", attrs={"id": BIN_DAYS_FORM_ID, "method": "post"})
+    return form
+
+
+def get_address_payload_from_form(address_form, address_identifier=None):
+    select = address_form.find("select")
+    name = select["name"]
+    options = select.find_all("option")
+    if address_identifier:
+        raise NotImplementedError(
+            "Address specific payload not implmented yet")
+    else:
+        value = options[-1]["value"]
+        print(
+            f'Using default address: {options[-1][""]}: {options[-1]["value"]}')
+    return {name: value}
+
+
+def fetch_bin_days_page(address_form, address_identifier=None):
+    payload = get_payload_from_form(address_form)
+    address_payload = get_address_payload_from_form(
+        address_form, address_identifier)
+    payload.update(address_payload)
+    # response = session.post(address_form["action"], data=payload)
+    # with open("./fetch_bin_days_response.pk", 'wb') as f:
+        # pickle.dump(response, f)
+    with open("./fetch_bin_days_response.pk", 'rb') as f:
+        response = pickle.dump(f)
+    a = 1
 
 
 def parse_args():
@@ -49,6 +90,7 @@ def validate_postcode(postcode):
 def get_current_pdf_url(postcode):
     post_code_form = fetch_postcode_form()
     address_form = fetch_address_form(post_code_form, postcode)
+    bin_days_page = fetch_bin_days_page(address_form)
 
 
 def main(postcode):
